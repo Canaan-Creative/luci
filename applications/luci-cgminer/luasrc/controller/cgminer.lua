8d2e846b6b0a956ad47f09d49a1d5b67e8a38e06
Xiangfu <xiangfu@openmobilefree.net>
2013-01-25 01:51:05 +0800

    display some text

--- applications/luci-cgminer/luasrc/controller/cgminer.lua
+++ applications/luci-cgminer/luasrc/controller/cgminer.lua
@@ -1,6 +1,14 @@
 module("luci.controller.cgminer", package.seeall)
 
 function index()
-   entry({"admin", "status", "cgminer"}, cbi("cgminer/cgminer"), _("Cgminer")).dependent=true
+   entry({"admin", "status", "cgminer"}, cbi("cgminer/cgminer"), _("Cgminer Configuration"))
+   entry({"admin", "status", "cgminerstatus"}, call("action_cstatus"), _("Cgminer Status"))
 end
 
+function action_cstatus()
+    local pp   = io.popen("/usr/bin/cgminer-api; /usr/bin/cgminer-api devs; /usr/bin/cgminer-api pools; /usr/bin/cgminer-api stats")
+    local data = pp:read("*a")
+    pp:close()
+
+    luci.template.render("cgminerstatus", {cstatus=data})
+end
