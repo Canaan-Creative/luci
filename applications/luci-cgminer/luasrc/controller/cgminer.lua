e4f0a194721496d96ea804d09a991e7e42d4e89a
Xiangfu <xiangfu@openmobilefree.net>
2013-02-23 17:58:15 +0800

    rename cgmienrstatus to cgminer api

--- applications/luci-cgminer/luasrc/controller/cgminer.lua
+++ applications/luci-cgminer/luasrc/controller/cgminer.lua
@@ -2,13 +2,13 @@ module("luci.controller.cgminer", package.seeall)
 
 function index()
    entry({"admin", "status", "cgminer"}, cbi("cgminer/cgminer"), _("Cgminer Configuration"))
-   entry({"admin", "status", "cgminerstatus"}, call("action_cstatus"), _("Cgminer Status"))
+   entry({"admin", "status", "cgminerapi"}, call("action_cgminerapi"), _("Cgminer API Log"))
 end
 
-function action_cstatus()
+function action_cgminerapi()
     local pp   = io.popen("/usr/bin/cgminer-api; /usr/bin/cgminer-api devs; /usr/bin/cgminer-api pools; /usr/bin/cgminer-api stats")
     local data = pp:read("*a")
     pp:close()
 
-    luci.template.render("cgminerstatus", {cstatus=data})
+    luci.template.render("cgminerapi", {api=data})
 end
