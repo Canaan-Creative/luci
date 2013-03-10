efdb1ba34ccf880e603397c044a08806f90c8dcc
Xiangfu <xiangfu@openmobilefree.net>
2013-03-10 09:59:14 +0800

    cgminer: add avalon chip frequency and modular count options

--- applications/luci-cgminer/luasrc/model/cbi/cgminer/cgminer.lua
+++ applications/luci-cgminer/luasrc/model/cbi/cgminer/cgminer.lua
@@ -1,19 +1,30 @@
-m = Map("cgminer", "Configuration", "")
+m = Map("cgminer", translate("Configuration"),
+	translate("The cgminer pool/parameter settings for Avalon ASIC Bitcoin mining machine."))
 
 conf = m:section(TypedSection, "cgminer", "")
 conf.anonymous = true
 conf.addremove = false
 
-conf:tab("default", translate("General Settings"))
+pool1url = conf:option(Value, "pool1url", translate("Pool 1"))
+pool1user = conf:option(Value, "pool1user", translate("Pool1 worker"))
+pool1pw = conf:option(Value, "pool1pw", translate("Pool1 password"))
+pool2url = conf:option(Value, "pool2url", translate("Pool 2"))
+pool2user = conf:option(Value, "pool2user", translate("Pool2 worker"))
+pool2pw = conf:option(Value, "pool2pw", translate("Pool2 password"))
+pool3url = conf:option(Value, "pool3url", translate("Pool 3"))
+pool3user = conf:option(Value, "pool3user", translate("Pool3 worker"))
+pool3pw = conf:option(Value, "pool3pw", translate("Pool3 password"))
 
-pool1url = conf:taboption("default", Value, "pool1url", translate("Pool 1"))
-pool1user = conf:taboption("default", Value, "pool1user", translate("Pool1 worker"))
-pool1pw = conf:taboption("default", Value, "pool1pw", translate("Pool1 password"))
-pool2url = conf:taboption("default", Value, "pool2url", translate("Pool 2"))
-pool2user = conf:taboption("default", Value, "pool2user", translate("Pool2 worker"))
-pool2pw = conf:taboption("default", Value, "pool2pw", translate("Pool2 password"))
-pool3url = conf:taboption("default", Value, "pool3url", translate("Pool 3"))
-pool3user = conf:taboption("default", Value, "pool3user", translate("Pool3 worker"))
-pool3pw = conf:taboption("default", Value, "pool3pw", translate("Pool3 password"))
+cf = conf:option(ListValue, "chip_frequency", translate("Chip Frequency(Default: 282M)"))
+cf.default = "282"
+cf:value("256", translate("256M"))
+cf:value("270", translate("270M"))
+cf:value("282", translate("282M"))
+cf:value("300", translate("300M"))
+
+mc = conf:option(ListValue, "miner_count", translate("Modular Count(Default: 3)"))
+mc.default = "24"
+mc:value("24", translate("3"))
+mc:value("32", translate("4"))
 
 return m
