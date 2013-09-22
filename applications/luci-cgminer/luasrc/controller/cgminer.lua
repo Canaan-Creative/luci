e0f49e9b32537078aae263087c35a16bc7493b94
Xiangfu <xiangfu@openmobilefree.net>
2013-09-22 18:03:23 +0800

    fix typo

--- applications/luci-cgminer/luasrc/controller/cgminer.lua
+++ applications/luci-cgminer/luasrc/controller/cgminer.lua
@@ -111,7 +111,7 @@ function devs()
    for line in devs do
       local fv = fver:match("(.*)")
       local status, mhs5s = line:match("Status=(%a+).*MHS 5s=([%d%.]+)")
-      local mc, ac, f, f1, f2, f3, t1, t2, t3, dw, nmw = line:match("miner_count=(%d+),asic_count=(%d+),.*,frequency=(%d+),fan1=(%d+),fan2=(%d+),fan3=(%d+),temp1=([%-%d]+),temp2=([%-%d]+),temp3=([%-%d]+),.*Device Hardware%%=([%d%.]+),no_matching_work=(%d+)")
+      local mc, ac, f, f1, f2, f3, t1, t2, t3, dh, nmw = line:match("miner_count=(%d+),asic_count=(%d+),.*,frequency=(%d+),fan1=(%d+),fan2=(%d+),fan3=(%d+),temp1=([%-%d]+),temp2=([%-%d]+),temp3=([%-%d]+),.*Device Hardware%%=([%d%.]+),no_matching_work=(%d+)")
       if mhs5s then
 	 st = status
 	 m5 = mhs5s
@@ -129,7 +129,7 @@ function devs()
 	    ['temp1'] = t1,
 	    ['temp2'] = t2,
 	    ['temp3'] = t3,
-	    ['dw'] = dw,
+	    ['dh'] = dh,
 	    ['nmw'] = nmw,
 	    ['fv'] = fv
 	 }
