3ea4ab4a670e5dce9103c8e1a90de97c663de735
Xiangfu <xiangfu@openmobilefree.net>
2014-01-27 18:21:26 +0800

    add new devs section

--- applications/luci-cgminer/luasrc/controller/cgminer.lua
+++ applications/luci-cgminer/luasrc/controller/cgminer.lua
@@ -46,7 +46,7 @@ function summary()
 	    localwork, remotefailures, networkblocks, totalmh, wu,
 	    diffaccepted, diffrejected, diffstale, bestshare =
 	       line:match(".*," ..
-	       		  "Elapsed=(%d+)," .. 
+	       		  "Elapsed=(%d+)," ..
 	       		  "MHS av=([%d%.]+)," ..
 	       		  ".*," ..
 	       		  "Found Blocks=(%d+)," ..
@@ -123,17 +123,135 @@ function summary()
    return data
 end
 
-function devs()
-   local st, m5, fv
+function pools()
    local data = {}
+   local pools = luci.util.execi("/usr/bin/cgminer-api -o pools | sed \"s/|/\\n/g\" ")
 
-   local devs = luci.util.execi("/usr/bin/cgminer-api -o stats | sed \"s/|/\\n/g\" | grep AV2 ")
+   if not pools then
+      return
+   end
+
+   for line in pools do
+      local pi, url, st, pri, quo, lp, gw, a, r, dc, sta, gf,
+      	    rf, user, lst, ds, da, dr, dsta, lsd, hs, sa, su, hg =
+	 line:match("POOL=(%d+)," ..
+	 	    "URL=(.*)," ..
+	 	    "Status=(%a+)," ..
+	 	    "Priority=(%d+)," ..
+	 	    "Quota=(%d+)," ..
+	 	    "Long Poll=(%a+)," ..
+	 	    "Getworks=(%d+)," ..
+	 	    "Accepted=(%d+)," ..
+	 	    "Rejected=(%d+)," ..
+	 	    ".*," ..
+	 	    "Discarded=(%d+)," ..
+	 	    "Stale=(%d+)," ..
+	 	    "Get Failures=(%d+)," ..
+	 	    "Remote Failures=(%d+)," ..
+	 	    "User=(.*)," ..
+	 	    "Last Share Time=(%d+)," ..
+	 	    "Diff1 Shares=(%d+)," ..
+	 	    ".*," ..
+	 	    "Difficulty Accepted=(%d+)[%.%d]+," ..
+	 	    "Difficulty Rejected=(%d+)[%.%d]+," ..
+	 	    "Difficulty Stale=(%d+)[%.%d]+," ..
+	 	    "Last Share Difficulty=(%d+)[%.%d]+," ..
+	 	    "Has Stratum=(%a+)," ..
+	 	    "Stratum Active=(%a+)," ..
+	 	    "Stratum URL=.*," ..
+	 	    "Has GBT=(%a+)")
+      if pi then
+	 if lst == "0" then
+	    lst_date = "Never"
+	 else
+	    lst_date = os.date("%c", lst)
+	 end
+	 data[#data+1] = {
+	    ['pool'] = pi,
+	    ['url'] = url,
+	    ['status'] = st,
+	    ['priority'] = pri,
+	    ['quota'] = quo,
+	    ['longpoll'] = lp,
+	    ['getworks'] = gw,
+	    ['accepted'] = a,
+	    ['rejected'] = r,
+	    ['discarded'] = dc,
+	    ['stale'] = sta,
+	    ['getfailures'] = gf,
+	    ['remotefailures'] = rf,
+	    ['user'] = user,
+	    ['lastsharetime'] = lst_date,
+	    ['diff1shares'] = ds,
+	    ['diffaccepted'] = da,
+	    ['diffrejected'] = dr,
+	    ['diffstale'] = dsta,
+	    ['lastsharedifficulty'] = lsd,
+	    ['hasstratum'] = hs,
+	    ['stratumactive'] = sa,
+	    ['stratumurl'] = su,
+	    ['hasgbt'] = hg
+	 }
+      end
+   end
+
+   return data
+end
+
+function devs()
+   local data = {}
+   local devs = luci.util.execi("/usr/bin/cgminer-api -o devs | sed \"s/|/\\n/g\" ")
 
    if not devs then
       return
    end
 
    for line in devs do
+      local asc, name, id, enabled, status, temp, mhsav, mhs5s, lvw, dh =
+	 line:match("ASC=(%d+)," ..
+		    "Name=([%a%d]+)," ..
+		    "ID=(%d+)," ..
+		    "Enabled=(%a+)," ..
+		    "Status=(%a+)," ..
+		    "Temperature=([%.%d]+)," ..
+		    "MHS av=([%.%d]+)," ..
+		    "MHS 5s=([%.%d]+)," ..
+		    ".*," ..
+		    "Last Valid Work=(%d+)," ..
+		    "Device Hardware%%=([%.%d]+)")
+
+	if lvw == "0" then
+	    lvw_date = "Never"
+	else
+	    lvw_date = os.date("%c", lst)
+	end
+
+	if asc then
+	    data[#data+1] = {
+	       ['name'] = "ASC" .. asc .. "-" .. name .. "-" .. id,
+	       ['enable'] = enabled,
+	       ['status'] = status,
+	       ['temp'] = temp,
+	       ['mhsav'] = mhsav,
+	       ['mhs5s'] = mhs5s,
+	       ['lvw'] = lvw_date
+	    }
+	end
+   end
+
+   return data
+end
+
+function stats()
+   local data = {}
+
+   local stats = luci.util.execi("/usr/bin/cgminer-api -o stats | sed \"s/|/\\n/g\" | grep AV2 ")
+
+   if not stats then
+      return
+   end
+
+   for line in stats do
       local id,
       	    id1, id2, id3,
       	    lw1, lw2, lw3,
@@ -209,81 +327,6 @@ function devs()
 	    }
 
 	 end
-   end
-
-   return data
-end
-
-function pools()
-   local data = {}
-   local pools = luci.util.execi("/usr/bin/cgminer-api -o pools | sed \"s/|/\\n/g\" ")
-
-   if not pools then
-      return
-   end
-
-   for line in pools do
-      local pi, url, st, pri, quo, lp, gw, a, r, dc, sta, gf, 
-      	    rf, user, lst, ds, da, dr, dsta, lsd, hs, sa, su, hg =
-	 line:match("POOL=(%d+)," ..
-	 	    "URL=(.*)," ..
-	 	    "Status=(%a+)," ..
-	 	    "Priority=(%d+)," ..
-	 	    "Quota=(%d+)," ..
-	 	    "Long Poll=(%a+)," ..
-	 	    "Getworks=(%d+)," ..
-	 	    "Accepted=(%d+)," ..
-	 	    "Rejected=(%d+)," ..
-	 	    ".*," ..
-	 	    "Discarded=(%d+)," ..
-	 	    "Stale=(%d+)," ..
-	 	    "Get Failures=(%d+)," ..
-	 	    "Remote Failures=(%d+)," ..
-	 	    "User=(.*)," ..
-	 	    "Last Share Time=(%d+)," ..
-	 	    "Diff1 Shares=(%d+)," ..
-	 	    ".*," ..
-	 	    "Difficulty Accepted=(%d+)[%.%d]+," ..
-	 	    "Difficulty Rejected=(%d+)[%.%d]+," ..
-	 	    "Difficulty Stale=(%d+)[%.%d]+," ..
-	 	    "Last Share Difficulty=(%d+)[%.%d]+," ..
-	 	    "Has Stratum=(%a+)," ..
-	 	    "Stratum Active=(%a+)," ..
-	 	    "Stratum URL=.*," ..
-	 	    "Has GBT=(%a+)")
-      if pi then
-	 if lst == "0" then
-	    lst_date = "Never"
-	 else
-	    lst_date = os.date("%c", lst)
-	 end
-	 data[#data+1] = {
-	    ['pool'] = pi,
-	    ['url'] = url,
-	    ['status'] = st,
-	    ['priority'] = pri,
-	    ['quota'] = quo,
-	    ['longpoll'] = lp,
-	    ['getworks'] = gw,
-	    ['accepted'] = a,
-	    ['rejected'] = r,
-	    ['discarded'] = dc,
-	    ['stale'] = sta,
-	    ['getfailures'] = gf,
-	    ['remotefailures'] = rf,
-	    ['user'] = user,
-	    ['lastsharetime'] = lst_date,
-	    ['diff1shares'] = ds,
-	    ['diffaccepted'] = da,
-	    ['diffrejected'] = dr,
-	    ['diffstale'] = dsta,
-	    ['lastsharedifficulty'] = lsd,
-	    ['hasstratum'] = hs,
-	    ['stratumactive'] = sa,
-	    ['stratumurl'] = su,
-	    ['hasgbt'] = hg
-	 }
-      end
    end
 
    return data
