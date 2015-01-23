2e709a2c4c641e6b179cfd1128f5f111eb167949
forkye <fuyepku@gmail.com>
2015-01-23 15:09:18 +0800

    Add pool api for reading hashrate from pool (stage 2)

--- applications/luci-avalon/luasrc/controller/avalon.lua
+++ applications/luci-avalon/luasrc/controller/avalon.lua
@@ -36,20 +36,20 @@ end
 
 --[[curl = require "luacurl" 
 
-function get_html(url, c) 
-    local result = { } 
-    if c == nil then 
-        c = curl.new() 
-    end 
-    c:setopt(curl.OPT_URL, url) 
-    c:setopt(curl.OPT_WRITEDATA, result) 
+function get_html(url, c)
+    local result = { }
+    if c == nil then
+        c = curl.new()
+    end
+    c:setopt(curl.OPT_URL, url)
+    c:setopt(curl.OPT_WRITEDATA, result)
     c:setopt(curl.OPT_WRITEFUNCTION, function(tab, buffer)
         table.insert(tab, buffer)
 
-        return #buffer                         
-    end) 
-    local ok = c:perform() 
-    return ok, table.concat(result) 
+        return #buffer
+    end)
+    local ok = c:perform()
+    return ok, table.concat(result)
 end ]]--
 
 function api_getpoolstatus()
