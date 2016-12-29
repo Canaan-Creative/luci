de667e007740b00c78f9f0e75d21307b8ff8f1c7
Mikeqin <Fengling.Qin@gmail.com>
2016-12-29 15:23:31 +0800

    Add default stratum pool url (HaoBTC)

--- applications/luci-app-avalon/luasrc/model/cbi/cgsetting.lua
+++ applications/luci-app-avalon/luasrc/model/cbi/cgsetting.lua
@@ -15,6 +15,9 @@ pool1section = m:section(TypedSection, "cgminer", translate("Pool1"))
 pool1section.template = "cgsetting/cbi_tblsection"
 pool1section.anonymous = true
 pool1url = pool1section:option(Value, "pool1url", translate("Pool 1 url"))
+pool1url.datatype = "string"
+pool1url:value("stratum+tcp://stratum.kano.is:3333")
+pool1url:value("stratum+tcp://stratum.haobtc.com:3333")
 pool1user = pool1section:option(Value, "pool1user", translate("Pool1 worker"))
 pool1pw = pool1section:option(Value, "pool1pw", translate("Pool1 password"))
 
@@ -22,6 +25,9 @@ pool2section = m:section(TypedSection, "cgminer", translate("Pool2"))
 pool2section.template = "cgsetting/cbi_tblsection"
 pool2section.anonymous = true
 pool2url = pool2section:option(Value, "pool2url", translate("Pool 2 url"))
+pool2url.datatype = "string"
+pool2url:value("stratum+tcp://stratum80.kano.is:80")
+pool2url:value("stratum+tcp://stratum.haobtc.com:443")
 pool2user = pool2section:option(Value, "pool2user", translate("Pool2 worker"))
 pool2pw = pool2section:option(Value, "pool2pw", translate("Pool2 password"))
 
@@ -29,20 +35,10 @@ pool3section = m:section(TypedSection, "cgminer", translate("Pool3"))
 pool3section.template = "cgsetting/cbi_tblsection"
 pool3section.anonymous = true
 pool3url = pool3section:option(Value, "pool3url", translate("Pool 3 url"))
+pool3url.datatype = "string"
+pool3url:value("stratum+tcp://stratum80.kano.is:81")
+pool3url:value("stratum+tcp://stratum.haobtc.com:25")
 pool3user = pool3section:option(Value, "pool3user", translate("Pool3 worker"))
 pool3pw = pool3section:option(Value, "pool3pw", translate("Pool3 password"))
-
---machinesection = m:section(TypeSection, "cgminer", translate("Machine"))
---machinesection.template = "cgsetting/cbi_tblsection"
---machinesection.anonymous = true
---chip_frequency = machinesection:option(Value, "chip_frequency", translate("Chip Frequency(Avalon2: 1500, Avalon3: 450)"))
---chip_voltage = machinesection:option(Value, "chip_voltage", translate("Chip Voltage(Avalon2: 10000, Avalon3: 6625)"))
---fan = machinesection:option(Value, "fan", translate("Fan%(Default: 90%)"))
-
--- etcsection = m:section(TypeSection, "cgminer", "")
--- etcsection.template = "cgsetting/cbi_tblsection"
--- etcsection.anonymous = true
--- api_allow = etcsection:option(Value, "api_allow", translate("API Allow(Default: W:127.0.0.1)"))
--- more_options = etcsection:option(Value, "more_options", translate("More Options(Default: --quiet)"))
 
 return m
--- applications/luci-app-cgminer/luasrc/model/cbi/cgminer/cgminer.lua
+++ applications/luci-app-cgminer/luasrc/model/cbi/cgminer/cgminer.lua
@@ -12,12 +12,21 @@ ntp:value("openwrt", translate("OpenWrt Default"))
 ntp:value("disable", translate("Disable"))
 
 pool1url = conf:option(Value, "pool1url", translate("Pool 1"))
+pool1url.datatype = "string"
+pool1url:value("stratum+tcp://stratum.kano.is:3333")
+pool1url:value("stratum+tcp://stratum.haobtc.com:3333")
 pool1user = conf:option(Value, "pool1user", translate("Pool1 worker"))
 pool1pw = conf:option(Value, "pool1pw", translate("Pool1 password"))
 pool2url = conf:option(Value, "pool2url", translate("Pool 2"))
+pool2url.datatype = "string"
+pool2url:value("stratum+tcp://stratum80.kano.is:80")
+pool2url:value("stratum+tcp://stratum.haobtc.com:443")
 pool2user = conf:option(Value, "pool2user", translate("Pool2 worker"))
 pool2pw = conf:option(Value, "pool2pw", translate("Pool2 password"))
 pool3url = conf:option(Value, "pool3url", translate("Pool 3"))
+pool3url.datatype = "string"
+pool3url:value("stratum+tcp://stratum80.kano.is:81")
+pool3url:value("stratum+tcp://stratum.haobtc.com:25")
 pool3user = conf:option(Value, "pool3user", translate("Pool3 worker"))
 pool3pw = conf:option(Value, "pool3pw", translate("Pool3 password"))
 
