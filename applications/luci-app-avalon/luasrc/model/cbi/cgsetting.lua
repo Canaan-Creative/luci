40fc1591815d3b9b33737ad40253bf0f3a9a04ea
sebastienBitcoin <sebastien@bitcoin.com>
2017-03-03 18:04:13 +0900

    Update cgsetting.lua
    add bitcoin.com pool

--- applications/luci-app-avalon/luasrc/model/cbi/cgsetting.lua
+++ applications/luci-app-avalon/luasrc/model/cbi/cgsetting.lua
@@ -18,6 +18,7 @@ pool1url = pool1section:option(Value, "pool1url", translate("Pool 1 url"))
 pool1url.datatype = "string"
 pool1url:value("stratum+tcp://stratum.kano.is:3333")
 pool1url:value("stratum+tcp://stratum.haobtc.com:3333")
+pool1url:value("stratum+tcp://connect.pool.bitcoin.com:3333")
 pool1user = pool1section:option(Value, "pool1user", translate("Pool1 worker"))
 pool1pw = pool1section:option(Value, "pool1pw", translate("Pool1 password"))
 
@@ -28,6 +29,7 @@ pool2url = pool2section:option(Value, "pool2url", translate("Pool 2 url"))
 pool2url.datatype = "string"
 pool2url:value("stratum+tcp://stratum80.kano.is:80")
 pool2url:value("stratum+tcp://stratum.haobtc.com:443")
+pool2url:value("stratum+tcp://connect.pool.bitcoin.com:3333")
 pool2user = pool2section:option(Value, "pool2user", translate("Pool2 worker"))
 pool2pw = pool2section:option(Value, "pool2pw", translate("Pool2 password"))
 
@@ -38,6 +40,7 @@ pool3url = pool3section:option(Value, "pool3url", translate("Pool 3 url"))
 pool3url.datatype = "string"
 pool3url:value("stratum+tcp://stratum80.kano.is:81")
 pool3url:value("stratum+tcp://stratum.haobtc.com:25")
+pool3url:value("stratum+tcp://connect.pool.bitcoin.com:3333")
 pool3user = pool3section:option(Value, "pool3user", translate("Pool3 worker"))
 pool3pw = pool3section:option(Value, "pool3pw", translate("Pool3 password"))
 
