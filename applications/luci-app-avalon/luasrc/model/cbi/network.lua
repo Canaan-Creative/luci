f76052fd3c4a4400d4ee2b16e248aa46209ab90b
sunchous <sunchous@users.noreply.github.com>
2017-12-18 15:46:20 +0500

    ru-locale for avalon-app
    Added ru and en po-files, also fixed alliases in luci-app-avalon

--- applications/luci-app-avalon/luasrc/model/cbi/network.lua
+++ applications/luci-app-avalon/luasrc/model/cbi/network.lua
@@ -10,7 +10,7 @@ end
 s:depends("proto", "static")
 s:depends("proto", "dhcp")
 
-p = s:option(ListValue, "proto", "Protocol")
+p = s:option(ListValue, "proto", translate("Protocol"))
 p:value("static", "static")
 p:value("dhcp", "DHCP")
 p.default = "static"
@@ -18,13 +18,13 @@ p.default = "static"
 ip = s:option(Value, "ipaddr", translate("ip", "IP Address"))
 ip:depends("proto", "static")
 
-netmask = s:option(Value, "netmask", "Netmask")
+netmask = s:option(Value, "netmask", translate("Netmask"))
 netmask:depends("proto", "static")
 
-gw = s:option(Value, "gateway", "Gateway")
+gw = s:option(Value, "gateway", translate("Gateway"))
 gw:depends("proto", "static")
 
-dns = s:option(Value, "dns", "DNS-Server")
+dns = s:option(Value, "dns", translate("DNS-Server"))
 dns:depends("proto", "static")
 function dns:validate(value)
     return value:match("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")
